2bb058917777954edd8c5c4a29a301951de2c88e
Marc Luiz Castro <111546344+CastroMarc@users.noreply.github.com>
2022-08-23 11:47:19 +0800

    Create Character.java

---
+++ Constructor/Character.java
@@ -0,0 +1,23 @@
+public class Character {
+
+    // Attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    // Method
+    public void sayMyName() {
+     System.out.println("\nHello I am " + name + "\nmy strength is: " + strength + "\nmy agility is: " + agility + "\nmy intelligence is: " + intelligence);
+
+    }
+
+    // Constructor
+    public Character(int str, int agi, int intel, String characterName) {
+        strength = str;
+        agility = agi;
+        intelligence = intel;
+        name = characterName;
+    }
+
+}
