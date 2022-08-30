271784b80cb5ec0cdcdb72c623f149ae55393555
Marc Luiz Castro <111546344+CastroMarc@users.noreply.github.com>
2022-08-30 10:16:53 +0800

    Create Gear.java

---
+++ Adding new object/Gear.java
@@ -0,0 +1,14 @@
+public class Gear {
+    // Gear attributes
+    String name;
+    String rarity;
+    int shield;
+    int weight;
+    boolean candodgemagicpotions;
+    String description;
+
+    // Gear method
+    public void sayMyName() {
+        System.out.println("The gear name is" + name + "and my shield value is" + shield);
+    }
+}
